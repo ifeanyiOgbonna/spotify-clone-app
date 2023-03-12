e4f808e9e26c8baeb225c53ebf4bbce1a450e0f0
Ifeanyi Ogbonna <ogbonna.ifybest@gmail.com>
2023-03-12 01:36:48 +0100

    app commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { LeftMenu } from './component/LeftMenu';
 import { MainMenu } from './component/MainMenu';
 import { RightMenu } from './component/RightMenu';
 
+
 function App() {
   return (
     <div className="App">
-    <LeftMenu />
-    <RightMenu/>
-    <MainMenu/>
+      <LeftMenu />
+      <RightMenu />
+      <MainMenu />
     </div>
   );
 }
